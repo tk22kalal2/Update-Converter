3d5d9d748e7baa64ea15ec43a32334eb6cdec120
Jack <125857863+Singhsawan@users.noreply.github.com>
2024-01-03 13:06:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,23 +9,23 @@ def is_enabled(value, default):
         return default
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID", 26726762))
-API_HASH = os.environ.get("API_HASH", '04c1514942a1fa624c461d1b0d61b85a')
-BOT_TOKEN = os.environ.get("BOT_TOKEN", '6698678257:AAEFx8qhbZYcP9O0V0OAcBr7RHa05GdKBUE')
+API_ID = int(os.environ.get("API_ID", 21748181))
+API_HASH = os.environ.get("API_HASH", 'b1d962414e186e0778911f3183feac33')
+BOT_TOKEN = os.environ.get("BOT_TOKEN", '6806119336:AAHPgsNAm6muevQanFDVxqKRQ-JFHlsrx5c')
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else []
-OWNER_ID = int(os.environ.get("OWNER_ID", "1252654109"))
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Shortener")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mdisky:ZdhPMoy0sq9JQ7t4@cluster0.otzltia.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5651594253"))
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "zxlink")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SawanSingh:sawansingh@cluster0.h2prshr.mongodb.net/?retryWrites=true&w=majority")
 # Optionnal variables
 BROADCAST_AS_COPY = is_enabled((os.environ.get('BROADCAST_AS_COPY', "False")), False)
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '')
-BIN_CHANNEL = os.environ.get("BIN_CHANNEL", -1002106262883)
+BIN_CHANNEL = os.environ.get("BIN_CHANNEL", -1001765670990)
 #  Replit Config
 REPLIT_USERNAME = os.environ.get("REPLIT_USERNAME", None)
 REPLIT_APP_NAME = os.environ.get("REPLIT_APP_NAME", None)
 REPLIT = f"https://{REPLIT_APP_NAME.lower()}.{REPLIT_USERNAME}.repl.co" if REPLIT_APP_NAME and REPLIT_USERNAME else False
 PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "300"))
-BASE_SITE = "mdisky.com"
+BASE_SITE = "zxlink.in"
 
 if OWNER_ID not in ADMINS:
     ADMINS.append(OWNER_ID)
