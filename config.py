19a8c4837acd4ea9cd2c3bee3254e2fb5d2d3538
Jack <125857863+Singhsawan@users.noreply.github.com>
2024-01-18 10:32:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ def is_enabled(value, default):
 # Mandatory variables for the bot to start
 API_ID = int(os.environ.get("API_ID", 21748181))
 API_HASH = os.environ.get("API_HASH", 'b1d962414e186e0778911f3183feac33')
-BOT_TOKEN = os.environ.get("BOT_TOKEN", '6806119336:AAHPgsNAm6muevQanFDVxqKRQ-JFHlsrx5c')
+BOT_TOKEN = os.environ.get("BOT_TOKEN", '6961619646:AAEDXbiZUvgiAi_UN-JezOpSVDWtNoKpvG0')
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else []
 OWNER_ID = int(os.environ.get("OWNER_ID", "5651594253"))
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "zxlink")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "urlpro")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SawanSingh:sawansingh@cluster0.h2prshr.mongodb.net/?retryWrites=true&w=majority")
 # Optionnal variables
 BROADCAST_AS_COPY = is_enabled((os.environ.get('BROADCAST_AS_COPY', "False")), False)
@@ -25,7 +25,7 @@ REPLIT_USERNAME = os.environ.get("REPLIT_USERNAME", None)
 REPLIT_APP_NAME = os.environ.get("REPLIT_APP_NAME", None)
 REPLIT = f"https://{REPLIT_APP_NAME.lower()}.{REPLIT_USERNAME}.repl.co" if REPLIT_APP_NAME and REPLIT_USERNAME else False
 PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "300"))
-BASE_SITE = "zxlink.in"
+BASE_SITE = "urlpro.net"
 
 if OWNER_ID not in ADMINS:
     ADMINS.append(OWNER_ID)
