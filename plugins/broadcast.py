d4c64acc2100b43e7a55d8edb69e4eebc99f0499
Akhand Bharat <125857863+Singhsawan@users.noreply.github.com>
2024-04-30 12:02:05 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,5 +1,3 @@
-# Don't Edit
-
 import asyncio
 import datetime
 import logging
@@ -94,3 +92,16 @@ async def main_broadcast_handler(m: Message):
         await m.reply_document(document='broadcast.txt', caption=f"Broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
 
     await aiofiles.os.remove('broadcast.txt')
+"""
+   _____                    __  __         _    _              _       _______           _     
+  / ____|                  |  \/  |       | |  | |            ( )     |__   __|         | |    
+ | |  __  _ __  ___  _   _ | \  / |  __ _ | |_ | |_  ___  _ __|/ ___     | |  ___   ___ | |__  
+ | | |_ || '__|/ _ \| | | || |\/| | / _` || __|| __|/ _ \| '__| / __|    | | / _ \ / __|| '_ \ 
+ | |__| || |  |  __/| |_| || |  | || (_| || |_ | |_|  __/| |    \__ \    | ||  __/| (__ | | | |
+  \_____||_|   \___| \__, ||_|  |_| \__,_| \__| \__|\___||_|    |___/    |_| \___| \___||_| |_|
+                      __/ |                                                                    
+                     |___/                                                                     
+Author: GreyMatter's Tech
+GitHub: https://GreyMattersTech.com/GitHub
+Website: https://GreyMattersTech.com
+"""
